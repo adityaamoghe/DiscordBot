a505ad273e3cc9888489312263abafd8bcef8e01
Aaditya <aaprasad@ucsd.edu>
2021-02-20 17:09:15 -0800

    Wrote add and classes commands

--- main.js
+++ main.js
@@ -3,9 +3,8 @@
 */
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 const prefix = '!';
-
+var class_list = [];
 client.once('ready', () => {
     console.log('ZoomBot is ready!');
 });
@@ -34,8 +33,22 @@ client.on('message', message =>{
         message.channel.send('3.    Class Three');
         
     }
-
-
+    else if (command == "add"){
+        if(!args.length){
+            return message.channel.send("Please provide the class name and zoom link you would like to add");
+        }
+        else{
+            var class_x = {Name: args[0], Link: args[1]}
+            class_list.push(class_x);
+            message.channel.send(`Class Added: ${args[0]}`);
+        }
+    }
+    else if (command == "classes"){
+        for (x in class_list){
+            message.channel.send(class_list[x]);
+        }
+    }
+    //process.exit();
 });
 
 client.login('ODEyNzczMjk1OTg3NjIxOTI4.YDFoHg.sXcMgv071mGRVKFYdQ7OCLEcIv8');
